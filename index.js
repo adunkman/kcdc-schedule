e5c2e4308914a53cdb164da02590ec4176457f54
Andrew Dunkman <adunkman@Benny.local>
2012-04-27 10:45:53 -0500

    CSV is now read to view

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var express = require("express"),
-    app = express.createServer();
+    app = express.createServer(),
+    csv = require("csv"),
+    scheduleFile = __dirname + "/data/schedule.csv",
+    port = 8080;
 
+app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  var events = [];
+
+  csv()
+    .fromPath(scheduleFile)
+    .on("data", function (data) {
+      events.push(data);
+    })
+    .on("end", function () {
+      res.render("index", { events: events });
+    });
 });
+
+app.listen(port);
